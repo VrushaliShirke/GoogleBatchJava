51d53aa1d00bb499db20901d4e96b9b59039a0b2
SHIRKE <SHIRKE@DESKTOP-5T1R4A7>
2022-10-17 21:51:36 +0530

    pushcode

--- src/com/day35/LongestWord.java
+++ src/com/day35/LongestWord.java
@@ -4,21 +4,15 @@ public class LongestWord {
 	
 	public static void longestWord(String str) {
 	
-		String s[]= str.split(" ");
-		String longestString="";
-		
-		for(int i=0;i<s.length;i++) {
-			for(int j=i+1;i<s.length;i++) {
-				if(s[i].length()>=s[j].length()) {
-					longestString=s[i];
-					
-					System.out.println(longestString+" = "+s[i].length()+" characters.");
-					
+		String largestWord="";
+		String s[]= str.split("\\s");
+		for(int i=0;i<s.length;i++) {	
+				if(s[i].length()>=largestWord.length()) {
+					largestWord=s[i];
 				}
-			}
 		}
+		System.out.println("Largest word is: "+largestWord+" & Length of word is = "+largestWord.length());
 	}
-
 	public static void main(String[] args) {
 		
 		String s= "India is my country";
